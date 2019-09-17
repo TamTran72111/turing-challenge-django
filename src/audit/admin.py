24becf02165dd37d36ea31dfe526833562ab9df4
Shin72111 <tam.tran72111@gmail.com>
2019-09-17 10:10:14 -0400

    Install rest-auth and add models to the admin view

--- src/audit/admin.py
+++ src/audit/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Audit
+
+admin.site.register(Audit)
--- src/shipping/admin.py
+++ src/shipping/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shipping
+
+admin.site.register(Shipping)
